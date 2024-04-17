0a3aff973d6e89732b46e4ae76f27931db2e0c3d
Gabriel Vilela <gabrielvcr98@gmail.com>
2024-04-17 02:13:44 -0300

    default

---
+++ cypress/e2e/auth/auth.cy.js
@@ -0,0 +1,13 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+//teste teste teste
+
+describe ('teste de teste', () =>{
+  it ('pass', () => {
+    
+  })
+})
